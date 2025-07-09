714ac682ad79f655d1b2f9c3e18228bc96f3596a
Madson Mac <madsonmac@gmail.com>
2025-07-09 16:33:58 +0200

    feat: add coding keys to user model

--- ios/ios/Shared/Models/User.swift
+++ ios/ios/Shared/Models/User.swift
@@ -11,4 +11,10 @@ struct User: Codable, Identifiable {
     let id: Int
     let name: String
     let profilePictureUrl: URL
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case profilePictureUrl = "profile_picture_url"
+    }
 }
