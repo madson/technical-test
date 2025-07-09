0195101c5fc138a058c2c2f35df6b8cca5e41a0e
Madson Mac <madsonmac@gmail.com>
2025-07-09 17:58:41 +0200

    feat: initial stories view

--- ios/ios/StoriesFeature/Views/StoriesView.swift
+++ ios/ios/StoriesFeature/Views/StoriesView.swift
@@ -6,13 +6,55 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct StoriesView: View {
+    @ObservedObject var viewModel = StoriesViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(viewModel.storyPosts) { storyPost in
+            ZStack {
+                WebImage(url: storyPost.imageURL)  { image in
+                    image.resizable(capInsets: .init(top: 0, leading: 0, bottom: 0, trailing: 0), resizingMode: .stretch)
+                } placeholder: {
+                    Rectangle().foregroundColor(.gray)
+                }
+                .scaledToFill()
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height-73)
+
+                VStack() {
+                    Text("test")
+                        .multilineTextAlignment(.leading)
+                        .font(.caption)
+                        .foregroundColor(.primary)
+                    
+                    Spacer()
+
+                    Text(storyPost.content)
+                        .multilineTextAlignment(.leading)
+                        .font(.caption)
+                        .foregroundColor(.primary)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.init(top: 16, leading: 16, bottom: 60, trailing: 16))
+            }
+        }
+        .padding(0)
+        .contentMargins(0)
+        .background(.black)
+        .listStyle(PlainListStyle())
+        .listSectionSeparator(.hidden)
+        .listRowSeparator(.hidden)
+        .onAppear {
+            Task {
+                do {
+                    try await viewModel.fetchStoryPosts(after: 0)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    StoriesView()
+    StoriesView().environmentObject(StoriesViewModel())
 }
