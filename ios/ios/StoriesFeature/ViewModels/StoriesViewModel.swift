6a1560e3d4b11ac983dc7bc49064bb2287697b69
Madson Mac <madsonmac@gmail.com>
2025-07-09 17:58:18 +0200

    feat: change stories view model

--- ios/ios/StoriesFeature/ViewModels/StoriesViewModel.swift
+++ ios/ios/StoriesFeature/ViewModels/StoriesViewModel.swift
@@ -5,4 +5,19 @@
 //  Created by Madson Cardoso on 7/9/25.
 //
 
-import Foundation
+import SwiftUI
+
+final class StoriesViewModel: ObservableObject {
+    @Published var storyPosts: [StoryPost] = []
+    
+    private let storyPostRepository = StoryPostRepository()
+    
+    func fetchStoryPosts(after timestamp: Int) async throws {
+        do {
+            let storyPosts = try await storyPostRepository.fetchStoryPosts(after: timestamp)
+            self.storyPosts.append(contentsOf: storyPosts)
+        } catch {
+            throw error
+        }
+    }
+}
